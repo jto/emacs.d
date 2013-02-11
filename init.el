bb4058bbf8e69b5b5127ff8830cbbb7aafdc9387
Mike Prentice <mprentice@gmail.com>
2013-02-10 20:07:03 -0500

    Add package installs including emacs-starter-kit

--- init.el
+++ init.el
@@ -4,6 +4,29 @@
 ;; From http://a-nickels-worth.blogspot.com/2007/11/effective-emacs.html
 (defvar *emacs-load-start* (current-time))
 
+;; Initialize with packages. Most importantly, emacs-starter-kit for sane defaults.
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+;; Create a list of packages to install if not present.
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(defvar *my-packages* '()
+  "A list of packages to ensure are installed at launch.")
+(add-to-list '*my-packages* 'starter-kit)
+(add-to-list '*my-packages* 'starter-kit-lisp)
+(add-to-list '*my-packages* 'starter-kit-bindings)
+(add-to-list '*my-packages* 'starter-kit-eshell)
+(add-to-list '*my-packages* 'magit)
+(add-to-list '*my-packages* 'geiser)
+(add-to-list '*my-packages* 'graphviz-dot-mode)
+(add-to-list '*my-packages* 'autopair)
+(dolist (p *my-packages*)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 ;; Separate custom file.
 (when (not (featurep 'aquamacs))
   (setq custom-file "~/.emacs.d/emacs-custom.el")
