756ecd310cc19dd0e3b4bb4699cc496047b5f36f
Mike Prentice <mprentice@gmail.com>
2013-04-24 07:06:09 -0400

    Add ansi-term improvements

--- init.el
+++ init.el
@@ -326,6 +326,38 @@ Don't mess with special buffers."
 
 ;; ===============================================
 
+;; ansi-term improvements
+;; http://emacs-journey.blogspot.com/2012/06/improving-ansi-term.html
+
+(defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
+  (if (memq (process-status proc) '(signal exit))
+      (let ((buffer (process-buffer proc)))
+        ad-do-it
+        (kill-buffer buffer))
+    ad-do-it))
+;; (ad-activate 'term-sentinel)
+
+(defvar my-term-shell "/bin/bash")
+(defadvice ansi-term (before force-bash)
+  (interactive (list my-term-shell)))
+(ad-activate 'ansi-term)
+
+(defun my-term-use-utf8 ()
+  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+(add-hook 'term-exec-hook 'my-term-use-utf8)
+
+(defun my-term-paste (&optional string)
+ (interactive)
+ (process-send-string
+  (get-buffer-process (current-buffer))
+  (if string string (current-kill 0))))
+(defun my-term-hook ()
+  (goto-address-mode)
+  (define-key term-raw-map "\C-y" 'my-term-paste))
+(add-hook 'term-mode-hook 'my-term-hook)
+
+;; ===============================================
+
 ;; Separate custom file.
 (when (not (featurep 'aquamacs))
   (setq custom-file (expand-file-name "~/.emacs.d/emacs-custom.el"))
