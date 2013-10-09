d5b3837c433cd2d512b260f2b5e5ed74018e0c6a
Mike Prentice <mprentice@gmail.com>
2013-10-09 07:46:20 -0400

    Add sql auto upcase

--- init.el
+++ init.el
@@ -579,9 +579,45 @@ Default is true."
 
 ;; * SQL
 
+(require 'sql)
+
+;;; Make _ part of a word
+(modify-syntax-entry ?_ "w" sql-mode-syntax-table)
+
+(defcustom mike-sql-upcase-keywords t
+  "If non-nil will convert keywords to upper case in `mike-sql-electric-space'.")
+
+(defconst mike-sql-keywords
+  (map 'list 'symbol-name
+       '( select  as      from    where   group   by      sort    asc     desc
+                  between and     or      having  create  table   if      not
+                  exists  int     not     null    primary key     constraint
+                  delete  update ))
+  "Keywords to upcase")
+
+(defun mike-sql-interactive-mode-hook ()
+  (setq show-trailing-whitespace nil))
+
+(defun mike-sql-upcase-keyword ()
+  (when (and mike-sql-upcase-keywords
+             (member (current-word t) mike-sql-keywords))
+    (upcase-word -1)))
+
+(defun mike-sql-electric-space (&optional n)
+  (interactive)
+  (mike-sql-upcase-keyword)
+  (insert-char (string-to-char " ") (or n 1)))
+
+(define-key sql-mode-map (kbd "SPC") 'mike-sql-electric-space)
+(define-key sql-interactive-mode-map (kbd "SPC") 'mike-sql-electric-space)
+
+(add-hook 'sql-interactive-mode-hook 'mike-sql-interactive-mode-hook)
+
 ;; ** MySQL
 
 (add-to-list 'auto-mode-alist '("\\.mysql\\'" . sql-mode))
+
+;; ** Postgres
 
 
 ;; * ELISP
