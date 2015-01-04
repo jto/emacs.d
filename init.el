e20383b031b8dc06d7b44dcbc608170ebfa12321
Mike Prentice <mprentice@gmail.com>
2015-01-03 21:50:00 -0500

    Initial literate conversion of init.el

--- init.el
+++ init.el
@@ -1,45 +1,79 @@
-;; * init.el --- Mikemacs: Opinionated defaults for Mike's Emacs happiness.
+;;; #+TITLE: Mike Prentice's Emacs 24 Configuration
 
-;; * STARTUP
+;;; Mikemacs: Opinionated defaults for Mike's Emacs happiness.
 
-(require 'cl)
+;;; My attempt at a literate Emacs configuration. Structure and
+;;; contents borrowed liberally from URL
+;;; `http://www.aaronbedra.com/emacs.d/'. Also draws inspiration from
+;;; Emacs Prelude and Emacs Starter Kit.
+
+;;; init.el
+
+;;; * Startup
+
+;;; Keep track of Emacs start time in order to calculate how long it
+;;; takes to load. I use this both out of curiosity and to see if a
+;;; new configuration significantly increases/decreases my startup
+;;; time.
 
 (defvar mike-emacs-load-start-time (current-time)
   "The time at which Emacs was started.
 
 See URL `http://a-nickels-worth.blogspot.com/2007/11/effective-emacs.html'.")
 
-;; Use before-init.el to set variables to override defaults.
+;;; before-init.el, if it exists, will run before the configurations
+;;; listed here. Use it to override defaults and set machine or
+;;; environment specific parameters before libraries are loaded. For
+;;; example, `user-mail-address'.
+
 (let ((before-file (expand-file-name "before-init.el" user-emacs-directory)))
   (when (file-exists-p before-file)
     (load-file before-file)))
 
+;;; Starting in Emacs 24.3, the official documentation recommends we
+;;; require cl-lib instead of cl.
+
+;;; Package cl-lib gives us access to nifty macros and functions from
+;;; Common Lisp. My favorite is `pushnew'.
+
+(require 'cl-lib)
+
+;;; * User Details
+
+(setq user-full-name "Mike Prentice")
+
+;;; * Environment
+
+;;; Add site-lisp to the load path to pick up libraries I install
+;;; manually, outside of package management tools.
 
 (pushnew (expand-file-name "site-lisp" user-emacs-directory) load-path)
 
+;;; * Packages
 
-;; * PACKAGES
+;;; Include mikemacs package. I'll be reviewing the contained modules
+;;; and gradually incorporating them into this configuration file.
 
 (require 'mikemacs)
 
 
-;; * FINISH
+;;; * Finish
+
+;;; after-init.el, if it exists, will run after the configurations
+;;; listed here. Use it to override defaults and set machine or
+;;; environment specific parameters after libraries are loaded.
 
 (let ((after-file (expand-file-name "after-init.el" user-emacs-directory)))
   (when (file-exists-p after-file)
     (load-file after-file)))
 
-;; Final machine-specific settings.
+;;; Time Emacs startup complete. Calculate and display the number of
+;;; seconds it took to start up.
 
-;; Time Emacs startup complete.
 (message "Emacs startup in %ds"
          (let ((time (current-time)))
-           (let ((hi (first time))
-                 (lo (second time)))
-             (- (+ hi lo)
-                (+ (first mike-emacs-load-start-time)
-                   (second mike-emacs-load-start-time))))))
-
-(provide 'init)
-
-;; init.el ends here
+           (let ((current-hi (first time))
+                 (current-lo (second time))
+                 (startup-hi (first mike-emacs-load-start-time))
+                 (startup-lo (second mike-emacs-load-start-time)))
+             (- (+ current-hi current-lo) (+ startup-hi startup-lo)))))
